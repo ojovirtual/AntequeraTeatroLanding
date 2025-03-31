a256fbf213d6e39733a2a293a89547a7cfb63f1c
ojovirtual <ismomu@gmail.com>
2025-03-31 09:07:05 +0200

    Quitar imagenes innececesarias, cambio tamaño imágenes web, cambio diseño

--- main.js
+++ main.js
@@ -1,43 +1,75 @@
 function rellenaContenido() {
 	OBRAS.map(function (obra) {
 		PLANTILLA = `
-                <div class='column is-one-third'>
-                    <div class="card mb-2 h-100" style="opacity: 0.9; height: 100%; display: flex; flex-direction: column;">
-						<div class="card-image">
-							<figure class="image is-2by3">
-								${obra.url ? `<a href='https://www.giglon.com/evento/${obra.url}' target='_blank'><img src="${obra.img}" alt="${obra.titulo}" /></a>` : `<img src="${obra.img}" alt="${obra.titulo}" />`}
-							</figure>
-						</div>
-						<div class="card-content" style="flex-grow: 1;">
-							<div class="media">
-								<div class="media-content">
-									<p class="title is-4">${obra.titulo}</p>
-									<p class="subtitle is-6"><em>${obra.autor}</em></p>
-								</div>
-							</div>
-							<div class="content">                            
-								${obra.fechas.map((elemento) => {
-									return '<time>' + elemento + '</time> ';
-								})}
-								<p>
-									${obra.cuerpo}
-								</p>
-								<p class="has-text-weight-bold">Reparto:</p>
-								<p class="is-italic">${obra.reparto.join(', ')}</p>
-								<p><strong>Dirección:</strong> ${obra.director}</p>
-								<p>
-                                ${obra.tags.map((elemento) => {
-																	return '<span class="tag is-danger">' + elemento + '</span> ';
-																})}								
-                                </p>
-								${obra.url ? 
-									`<a class="button is-success" href="https://www.giglon.com/evento/${obra.url}" target="_blank">Comprar en Giglon.com</a>` : 
-									`<button class="button is-success" disabled>Entradas a la venta en breve</button>`
-								}
-                                
-							</div>                            
-						</div>
-					</div>
+                <div class='column is-4-desktop is-6-tablet is-12-mobile'>
+                    <div class="card mb-2">
+                        <div class="card-content">
+                            <div class="columns">
+                                <div class="column is-3">
+                                    <figure class="image is-2by3">
+                                        ${
+                                            obra.url
+                                                ? `<a href='https://www.giglon.com/evento/${obra.url}' target='_blank'><img src="${obra.img}" alt="${obra.titulo}" class="obra-image" /></a>`
+                                                : `<img src="${obra.img}" alt="${obra.titulo}" class="obra-image" />`
+                                        }
+                                    </figure>
+                                </div>
+                                <div class="column">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p class="title is-4">${obra.titulo}</p>
+                                            <p class="subtitle is-6"><em>${obra.autor}</em></p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <div class="tabs">
+                                            <ul>
+                                                <li class="is-active">
+                                                    <a>
+                                                        <span class="icon"><i class="fas fa-book"></i></span>
+                                                        <span>Sinopsis</span>
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a>
+                                                        <span class="icon"><i class="fas fa-users"></i></span>
+                                                        <span>Reparto</span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="tab-content">
+                                            <div class="tab-pane is-active" style="display: block;">
+                                                <div class="mb-2">
+                                                    ${obra.fechas.map((elemento) => {
+                                                        return '<time>' + elemento + '</time> ';
+                                                    })}
+                                                </div>
+                                                <p>${obra.cuerpo}</p>
+                                                <p><strong>Dirección:</strong> ${obra.director}</p>
+                                                <p>
+                                                    ${obra.tags.map((elemento) => {
+                                                        return '<span class="tag is-danger">' + elemento + '</span> ';
+                                                    })}
+                                                </p>
+                                            </div>
+                                            <div class="tab-pane" style="display: none;">
+                                                <p class="has-text-weight-bold">Reparto:</p>
+                                                <p class="is-italic">${obra.reparto.join(', ')}</p>
+                                            </div>
+                                        </div>
+                                        <div class="mt-4">
+                                            ${
+                                                obra.url
+                                                    ? `<a class="button is-success" href="https://www.giglon.com/evento/${obra.url}" target="_blank">Comprar en Giglon.com</a>`
+                                                    : `<button class="button is-success" disabled>Entradas a la venta en breve</button>`
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>`;
 		document.querySelector('div[name=contenedor]').innerHTML += PLANTILLA;
 	});
@@ -45,4 +77,59 @@ function rellenaContenido() {
 
 window.addEventListener('DOMContentLoaded', (event) => {
 	rellenaContenido();
+    
+    // Añadir funcionalidad a las pestañas
+    document.querySelectorAll('.tabs li').forEach(tab => {
+        tab.addEventListener('click', () => {
+            // Encontrar el contenedor de pestañas más cercano
+            const tabsContainer = tab.closest('.tabs');
+            const contentContainer = tabsContainer.nextElementSibling;
+            
+            // Obtener todas las pestañas y paneles dentro de este contenedor específico
+            const tabs = tabsContainer.querySelectorAll('li');
+            const panes = contentContainer.querySelectorAll('.tab-pane');
+            
+            // Remover clase activa de todas las pestañas y paneles
+            tabs.forEach(t => t.classList.remove('is-active'));
+            panes.forEach(p => {
+                p.classList.remove('is-active');
+                p.style.display = 'none';
+            });
+            
+            // Añadir clase activa a la pestaña seleccionada y su panel correspondiente
+            tab.classList.add('is-active');
+            const index = Array.from(tabs).indexOf(tab);
+            panes[index].classList.add('is-active');
+            panes[index].style.display = 'block';
+        });
+    });
+
+    // Funcionalidad del modal para las imágenes
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    const modalClose = modal.querySelector('.modal-close');
+    const modalBackground = modal.querySelector('.modal-background');
+
+    // Función para abrir el modal
+    function openModal(imageSrc) {
+        modalImage.src = imageSrc;
+        modal.classList.add('is-active');
+    }
+
+    // Función para cerrar el modal
+    function closeModal() {
+        modal.classList.remove('is-active');
+    }
+
+    // Event listeners para cerrar el modal
+    modalClose.addEventListener('click', closeModal);
+    modalBackground.addEventListener('click', closeModal);
+
+    // Event listener para las imágenes
+    document.querySelectorAll('.obra-image').forEach(img => {
+        img.addEventListener('click', (e) => {
+            e.preventDefault();
+            openModal(img.src);
+        });
+    });
 });
--- obras.js
+++ obras.js
@@ -2,7 +2,7 @@ var OBRAS = [
 	{
 		titulo: 'Cuatro corazones con freno y marcha atr&aacute;s',
 		autor: 'Jardiel Poncela',
-		img: 'img/corazones.png',
+		img: 'img/corazones_web.jpg',
 		cuerpo: `Una historia de eterna juventud, llena de personajes disparatados y momentos hilarantes. Nuestros protagonistas descubrir&aacute;n que vivir para siempre, no es sin&oacute;nimo de felicidad y tranquilidad. Las sorpresas se van sucediendo durante tres actos en los que los a&ntilde;os pasan con velocidad y la trama se complica cada vez m&aacute;s. Esperamos provocarles unas risas que les hagan rejuvenecer.`,
 		
 		fechas: ['17/05/2024 21:00h'],
@@ -13,7 +13,7 @@ var OBRAS = [
 	{
 		titulo: 'La herencia',
 		autor: 'Jean-Pierre Martínez',
-		img: 'img/herencia.jpeg',
+		img: 'img/herencia_web.jpg',
 		cuerpo: `La t&iacute;a Adela acaba de fallecer, y el legado de su maravilloso piso en el centro de Madrid, se convertir&aacute; en un problema para los futuros herederos. La comunidad del edificio, un tanto peculiar, se empe&ntilde;ar&aacute; en celebrar una fiesta de bienvenida, y es que estos vecinos, no aceptar&aacute;n un "no" por respuesta.`,
 
 		fechas: ['18/05/2024 21:00h'],
@@ -24,7 +24,7 @@ var OBRAS = [
 	{
 		titulo: 'No cabe un alma',
 		autor: 'Leandro Marcos',
-		img: 'img/alma.png',
+		img: 'img/alma_web.jpg',
 		cuerpo: `En esta supina burocracia celestial nos encontramos en un cielo abarrotado. &iquest;Qu&eacute; pasar&iacute;a si no hubiera espacio para ning&uacute;n alma m&aacute;s? &iquest;C&oacute;mo reaccionar&iacute;an las almas reci&eacute;n llegadas al cielo al ver qu&eacute; no pueden pasar la puerta celestial? En esta divertida comedia viajaremos al cielo para vivir mil y un enredos celestiales...`,
 
 		fechas: ['01/06/2024 21:00h'],
@@ -35,7 +35,7 @@ var OBRAS = [
 	{
 		titulo: 'La asamblea de las mujeres',
 		autor: 'Arist&oacute;fanes',
-		img: 'img/asamblea.jpeg',
+		img: 'img/asamblea_web.jpg',
 		cuerpo: `En esta comedia de Arist&oacute;fanes, las mujeres de Atenas se hacen con el poder de la ciudad, para demostrar a los hombres que son mejores gobernantes que ellos. Mujeres que se revelan contra ese servicial segundo plano al que estaban destinadas. 25 siglos nos separan a nosotras de ellas, sin embargo, &iquest;somos una sociedad diferente?`,
 
 		fechas: ['07/06/2024 21:00h'],
@@ -46,7 +46,7 @@ var OBRAS = [
 	{
 		titulo: 'La enferma imaginaria',
 		autor: 'Moli&egrave;re',
-		img: 'img/enferma.png',
+		img: 'img/enferma_web.jpg',
 		cuerpo: `En esta adaptaci&oacute;n del cl&aacute;sico de Moli&egrave;re, la enfermedad de Margot a pesar de las infinitas atenciones de su boticaria y su doctora, no remite. &iquest;Ser&aacute; capaz de sacrificar a su hija Ang&eacute;lica con tal de tener un m&eacute;dico en la familia?`,
 
 		fechas: ['08/06/2024 21:00h'],
@@ -57,7 +57,7 @@ var OBRAS = [
 	{
 		titulo: 'Angelina o el honor de un brigadier',
 		autor: 'Jardiel Poncela',
-		img: 'img/brigadier.png',
+		img: 'img/brigadier_web.jpg',
 		cuerpo: `La j&oacute;ven e inocente Angelina se ha prometido con Rodolfito, todo parece ir como la seda en esta familia de la alta sociedad de 1880, sin embargo, la paz, el sosiego y el amor, dar&aacute;n paso al caos y la deshonra del Brigadier, padre de Angelina. Jardiel Poncela nos presenta esta divertida comedia en verso que representa a modo de caricatura, la Espa&ntilde;a de 1880.`,
 		
 		fechas: ['15/06/2024 21:00h'],
@@ -68,7 +68,7 @@ var OBRAS = [
 	{
 		titulo: 'Criaturitas de Dios (Metafisi-clowns)',
 		autor: 'Tom&aacute;s Af&aacute;n',
-		img: 'img/clown.png',
+		img: 'img/clown_web.jpg',
 		cuerpo: `&iquest;Qui&eacute;nes somos, de d&oacute;nde venimos, ad&oacute;nde vamos? &iquest;Estamos solos en la galaxia o acompa&ntilde;ados? Estos divertidos payasos, que s&oacute;lo quieren jugar, nos traen la respuesta a todas nuestras dudas existenciales, o no. Amor, poder, religi&oacute;n, amistad, la vida y la muerte, todo tratado con el humor blanco de estas criaturitas teatrales.`,
 		
 		fechas: ['21/06/2024 21:00h'],
@@ -79,7 +79,7 @@ var OBRAS = [
 	{
 		titulo: 'Fuimos canciones',
 		autor: 'Elena Moreno',
-		img: 'img/fuimos_canciones.png',
+		img: 'img/fuimos_canciones_web.jpg',
 		cuerpo: `Macarena ha sido una pringada toda su vida, pero ahora est&aacute; mutando, se est&aacute; convirtiendo en una t&iacute;a guay. Despu&eacute;s de una ruptura sentimental ha estado recomponi&eacute;ndose, pero el mundo vuelve a tambalearse cuando Leo aparece de nuevo en la ciudad. En cada historia, el hilo argumental son las canciones, la BSO de nuestra vida. "Fuimos canciones" es esa historia de crecimiento personal.`,
 		
 		fechas: ['22/06/2024 21:00h'],
